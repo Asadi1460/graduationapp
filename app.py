e9346a8cdcc7ea31eb39a08d4c17658bf5b7b91e
Asadi1460 <eng.asadi@gmail.com>
2025-05-07 16:06:28 +0330

    update app.py

--- app.py
+++ app.py
@@ -1,17 +1,46 @@
 import streamlit as st
+from bs4 import BeautifulSoup
+import matplotlib.font_manager as fm
+import pandas as pd
 
-# آپلود فایل HTML و CSV
-# نمایش مشخصات دانشجو
-# نمایش تحلیل‌ها و DataFrameها
-# نمایش جدول پیش‌نیازها
-# دکمه برای تولید و دانلود PDF
+
+# uplaod CSV and HTML
+
+# Cache the data loading function
+@st.cache_data
+def load_data():
+    return pd.read_csv('data/courses.csv')
+
+# Check for remaining courses
+courses_df = load_data()
+
+# st.dataframe(courses_df.loc[:,::-1], hide_index=True, use_container_width=True)
 
 
 st.title("گزارش وضعیت تحصیلی دانشجو")
 
-# # بخش آپلود فایل‌ها
+# Upload Student Report .HTML
 # uploaded_html = st.file_uploader("آپلود فایل HTML", type="html")
+
+# Extract Student Info from HTML: Major, Stage, Entrance Year, ...
+# Set Major (Ap, Computer, ...)
+# Set Stage (Technical, Bachelor, ...)
+
+# Load Major-Stage info from main file .CSV
+# Extract info from Major-Stage main file .CSV like theoretical and practical units, ...
+
 # uploaded_csv = st.file_uploader("آپلود فایل دروس", type="csv")
+
+
+# Display Student Info
+# Display Selected lessons
+# Display Passed lessons
+# Display Remaining lessons
+# Display Violations
+
+# Display PDF Report
+# Download PDF Report
+
 
 # if uploaded_html and uploaded_csv:
 #     html = uploaded_html.read().decode("utf-8")
@@ -24,24 +53,4 @@ st.title("گزارش وضعیت تحصیلی دانشجو")
 #     df_remaining = get_remaining_courses(df_passed, course_db)
 #     df_violations = check_prerequisites(df_courses, df_passed, course_db)
 #     summary = summarize_units(df_courses)
-    
-#     # نمایش
-#     st.subheader("مشخصات دانشجو")
-#     st.json(student_info)
 
-    # st.subheader("دروس اخذ شده")
-    # st.dataframe(df_courses)
-
-    # st.subheader("دروس گذرانده")
-    # st.dataframe(df_passed)
-
-    # st.subheader("دروس باقی‌مانده")
-    # st.dataframe(df_remaining)
-
-    # st.subheader("جدول عدم رعایت پیش‌نیازها")
-    # st.dataframe(df_violations)
-
-    # if st.button("دریافت گزارش PDF"):
-    #     generate_pdf_report(student_info, summary, "summary.pdf")
-    #     with open("summary.pdf", "rb") as f:
-    #         st.download_button("دانلود PDF", f, file_name="report.pdf")
